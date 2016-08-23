30dbbad30bfc9aeed74d5ed221d931afc997021b
m00se333 <moosecunningham@gmail.com>
2016-08-23 12:23:28 -0700

    first commit, set up Node and imported old files from previous version

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/dist/index.html')
 });
