7d841a585335b96886d40a0b77e7644b14f6e447
m00se333 <moosecunningham@gmail.com>
2016-09-02 14:04:26 -0700

    partial URLs stored in the projects object save more space and I was able to get more space for the column displayed projects in portrait.

--- src/index.js
+++ src/index.js
@@ -213,8 +213,8 @@ var MasterLayout = React.createClass({
                 return(
                     <div className="project">
                       <h1>{this.props.id}</h1>
-                      <a href={data.url} target="blank">{data.url}</a>
-                      <a href={data.github} target="blank">{data.github}</a>
+                      <a href={"https://" + data.url} target="blank">{data.url}</a>
+                      <a href={"https://" + data.github} target="blank">{data.github}</a>
                       {this.formatParagraph()}
                     </div>
                   )
